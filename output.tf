66b57f0f809e99078e97eea68d4a7f6b5027a19f
knbob <email2nagababu@gmail.com>
2024-01-23 15:10:09 +0530

    terraform-aws-vpc

--- output.tf
+++ output.tf
@@ -6,3 +6,15 @@ output "vpc_id" {             # Output will be printed when source selected loca
     value = aws_vpc.main.id
   
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
